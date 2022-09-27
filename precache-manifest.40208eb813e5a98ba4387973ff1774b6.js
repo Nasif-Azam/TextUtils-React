be3fc8642ca3ae5a8af047add27d93f76c212072
Nasif-Azam <cr7nasif99@gmail.com>
2022-09-27 10:17:14 +0600

    Updates

---
+++ precache-manifest.40208eb813e5a98ba4387973ff1774b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff3e499408c2e8971fad",
+    "url": "/TextUtils-React/static/css/main.1f49ce33.chunk.css"
+  },
+  {
+    "revision": "ff3e499408c2e8971fad",
+    "url": "/TextUtils-React/static/js/main.aeccfe4d.chunk.js"
+  },
+  {
+    "revision": "499fd33915ffc16afe6d",
+    "url": "/TextUtils-React/static/js/runtime~main.912ca157.js"
+  },
+  {
+    "revision": "8873f49040e01505f779",
+    "url": "/TextUtils-React/static/js/2.ffd3fa5d.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/TextUtils-React/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "31550bb69dcb45cd1742aafd23ea1da1",
+    "url": "/TextUtils-React/index.html"
+  }
+];
